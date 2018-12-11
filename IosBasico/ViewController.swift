5d40cd649b48d18bc9e4cf5fc3cc1f5caaea1784
aluno <aluno@LABMAC18.local>
2018-12-10 21:48:59 -0300

    finalizado o jogo da memoria

--- IosBasico/ViewController.swift
+++ IosBasico/ViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contadorLabel: UILabel!
+    @IBOutlet var cards: [UIButton]!
+    
+    var emoticons = ["😡","😡","🤔","🤔"]
+    var contador = 0 {
+        didSet {
+            contadorLabel.text = "Flips: \(contador)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func clickEnois(_ sender: UIButton) {
-        print("Clicou")
+    @IBAction func flipCard(_ sender: UIButton) {
+        let cardIndex = cards.index(of: sender)
+        mainFlip(on: sender, withEmoji: emoticons[cardIndex!])
     }
     
+    func mainFlip(on btn: UIButton, withEmoji emoji: String){
+        contador += 1
+        if btn.currentTitle == emoji {
+            btn.setTitle("", for: UIControl.State.normal)
+        } else {
+            btn.setTitle(emoji, for: UIControl.State.normal)
+        }
+    }
 }
-
